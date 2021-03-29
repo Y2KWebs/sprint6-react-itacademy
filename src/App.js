cdc1a710c91ce3e85fb7b3ec116d1b9efe7c10cc
Manuel Congosto <mc@y2kwebs.com>
2021-03-29 12:18:59 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import Book from "./components/book";
-import data from "./data/frases.json";
+import logo from "./logo.svg";
 import "./App.css";
 
-const App = () => {
+function App() {
   return (
     <div className="App">
-      {data.map((e) => (
-        <Book title={e.title} author={e.author} />
-      ))}
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
+}
 
 export default App;
--- src/components/book.js
+++
@@ -1,9 +0,0 @@
-const Books = (props) => {
-  return (
-    <>
-      <h1>{props.title}</h1>
-      <h3>{props.author}</h3>
-    </>
-  );
-};
-export default Books;
